99b05b3be845d67644242cf7042b62c97e76b770
Bruno Gomes <bgomes.89@gmail.com>
2025-10-31 11:33:19 -0300

    fix: ajuste nome da pasta de prisma  para db

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,6 @@
 import express from 'express';
-import { prisma } from './prisma/prisma';
+import { prisma } from './db/prisma'
 
 export const app = express();
 
 app.use(express.json());
-
-
-app.get('/api', (req, res) => {
-  res.send('Hello, World!');
-});
